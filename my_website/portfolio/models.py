8ed084ecb48459805fbb3152d16edd7b04d50851
tprzybylek <tajemniczyjogurt@gmail.com>
2018-10-10 22:12:12 +0200

    Add feed_image to the AppPage class

--- my_website/portfolio/models.py
+++ my_website/portfolio/models.py
@@ -4,6 +4,7 @@ from modelcluster.fields import ParentalKey
 from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel, FieldRowPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
 
 
@@ -15,26 +16,42 @@ class AppsIndexPage(Page):
         context['apps'] = apps
         return context
 
-    parent_page_types = ['home.HomePage']
+    # parent_page_types = ['home.HomePage']
 
 
 class AppPage(Page):
     app_name = models.CharField(max_length=125)
+
+    feed_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
     github_url = models.URLField(blank=True)
     app_url = models.URLField(blank=True)
+    presentation_url = models.URLField(blank=True)
     intro = models.CharField(max_length=250)
     features = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('app_name'),
+
         MultiFieldPanel([
             FieldPanel('github_url'),
             FieldPanel('app_url'),
+            FieldPanel('presentation_url'),
         ], heading="App URLs"),
         MultiFieldPanel([
             FieldPanel('intro'),
             FieldPanel('features'),
         ], heading="App description"),
+    ]
+
+    promote_panels = Page.promote_panels + [
+        ImageChooserPanel('feed_image'),
     ]
 
     parent_page_types = ['portfolio.AppsIndexPage']
@@ -61,8 +78,12 @@ class ContactPage(AbstractEmailForm):
         ], "Email"),
     ]
 
-    parent_page_types = ['home.HomePage']
+    # parent_page_types = ['home.HomePage']
 
 
 class ResumePage(Page):
-    parent_page_types = ['home.HomePage']
+    # parent_page_types = ['home.HomePage']
+    pass
+
+class PortfolioHomePage(Page):
+    pass
